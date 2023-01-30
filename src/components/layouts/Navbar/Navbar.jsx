1c06b92680649db37be6fe75f3e6ab4cbb430b01
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-01-30 12:25:58 -0500

    se establecio nuevo sideBar

--- src/components/layouts/Navbar/Navbar.jsx
+++ src/components/layouts/Navbar/Navbar.jsx
@@ -1,47 +1,147 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import GamingLogo from '../../../image/GamingLogo.png'
-import $ from "jquery";
 
+export const Navbar = () => {
+    const [sidebarNav, setSidebarnav] = useState("close")
 
-  window.addEventListener("scroll", function(){
-    if(window.scrollY>=10){
-        document.querySelector('#Header').style.backgroundColor = 'rgba(0,0,0,0.7)';
-        document.querySelector('#Header').style.backdropFilter = 'blur(10px)';
-    }else{
-        document.querySelector('#Header').style.backgroundColor = 'transparent';
-        document.querySelector('#Header').style.backdropFilter = 'none';
+    const OpenMenu = () => {
+        setSidebarnav("showMenu")
     }
-  });
+    const CloseMenu = () => {
+        setSidebarnav("close")
+    }
 
-
-
-
-  function ChangeSignUp() {
-        
-  }
-  
-export const Navbar = () => (
-
-
+  return (
     <>
-        <div className="headerSection" id="Header">
-            <div className="logoContainer">
-                <img src='{}'  alt="" className="logo"/>
-            </div>
-            <div className="menuContainer">
-                <span className="menuItem"><a href="/" className="menuItemLink">Home</a></span>
-                <span className="menuItem"><a href="#" className="menuItemLink">Games</a></span>
-                <span className="menuItem"><a href="#" className="menuItemLink">News</a></span>
-                <span className="menuItem"><a href="#" className="menuItemLink">log in</a></span>
-                <span  onClick={ChangeSignUp}  className="menuItem"><a href="/sing/up" className="menuItemLink">Sign up</a></span>
-            </div>
+        <div className={`sidebar ${sidebarNav}`}>
+    <div class="logo-details">
+      {sidebarNav == "close" &&
+        <i onClick={OpenMenu} className='bx bx-menu' style={{cursor:"pointer"}} ></i>
+      }
 
-            <div className="hamburguerMenu">
-                <span className="line"></span>
-                <span className="line"></span>
-                <span className="line"></span>
-            </div>
-        </div> 
+      {sidebarNav == "showMenu" &&
+        <i onClick={CloseMenu} className='bx bx-menu' style={{cursor:"pointer"}} ></i>
+      }
+
+      <span className="logo_name">Market Tecnology</span>
+    </div>
+
+    <ul class="nav-links">
+      <li>
+        <a href="#">
+          <i class='bx bx-grid-alt' ></i>
+          <span class="link_name">Dashboard</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Category</a></li>
+        </ul>
+      </li>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class='bx bx-collection' ></i>
+            <span class="link_name">Category</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Category</a></li>
+          <li><a href="#">HTML & CSS</a></li>
+          <li><a href="#">JavaScript</a></li>
+          <li><a href="#">PHP & MySQL</a></li>
+        </ul>
+      </li>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class='bx bx-book-alt' ></i>
+            <span class="link_name">Posts</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Posts</a></li>
+          <li><a href="#">Web Design</a></li>
+          <li><a href="#">Login Form</a></li>
+          <li><a href="#">Card Design</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bx-pie-chart-alt-2' ></i>
+          <span class="link_name">Analytics</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Analytics</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bx-line-chart' ></i>
+          <span class="link_name">Chart</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Chart</a></li>
+        </ul>
+      </li>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class='bx bx-plug' ></i>
+            <span class="link_name">Plugins</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Plugins</a></li>
+          <li><a href="#">UI Face</a></li>
+          <li><a href="#">Pigments</a></li>
+          <li><a href="#">Box Icons</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bx-compass' ></i>
+          <span class="link_name">Explore</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Explore</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bx-history'></i>
+          <span class="link_name">History</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">History</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bx-cog' ></i>
+          <span class="link_name">Setting</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Setting</a></li>
+        </ul>
+      </li>
+      <li>
+    <div class="profile-details">
+      <div class="profile-content">
+        <img src={GamingLogo} alt={GamingLogo} />
+      </div>
+      <div class="name-job">
+        <div className="profile_name">Prem Shahi</div>
+        <div className="job">Web Desginer</div>
+      </div>
+      <i class='bx bx-log-out' ></i>
+    </div>
+  </li>
+</ul>
+  </div>
+
     </>
-)
+  )
+}
