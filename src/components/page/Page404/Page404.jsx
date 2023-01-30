cfc82568bd68071081ca1b9cd1db94783b94b358
dairongranada <dairongranada21@gmail.com>
2023-01-30 12:21:54 -0500

    Page 404 -DG

--- src/components/page/Page404/Page404.jsx
+++ src/components/page/Page404/Page404.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './404.css'
+import Monster404 from '../../../image/Monster404.svg'
 
 
 export const Page404 = () => {
   return (
     <>
       <div  className='backgroundContainer'></div>
+
+      <div className='Page404' >
+          <img src={Monster404} alt={Monster404} />
+           <a className='buttonHero' href="/">return home</a>
+      </div>
 
     </>
   )
