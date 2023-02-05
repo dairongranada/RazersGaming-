7e0ef2b9675b1c94b12fbf02899d740f9393d9f5
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-02-04 23:39:10 -0500

    formularios de registro y logeo terminados temporalmente, falta funcionalidad

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -22,7 +22,7 @@ function App() {
     <Routes>
           <Route path='/' element= {<PageHome />} />
           <Route path="*" element= {<Page404 />} />
-          <Route path="sing/up" element= {<SingUp />} />
+          <Route path="/singUp" element= {<SingUp />} />
 
     </Routes>
     <Footer />
