bb7c76122da787791d914b0030fb48f18b873af0
dairongranada <dairongranada21@gmail.com>
2023-02-06 17:40:02 -0500

    update changes -DG

--- src/components/layouts/Navbar/Navbar.jsx
+++ src/components/layouts/Navbar/Navbar.jsx
@@ -28,11 +28,19 @@ export const Navbar = () => {
     </div>
 
     <ul class="nav-links">
+
       <li>
-        <a href="#">
-          <i class='bx bx-grid-alt' ></i>
-          <span class="link_name">Dashboard</span>
+        <a href="/">
+          <i class='bx bx-home-circle' ></i>
+          <span class="link_name">Home</span>
         </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="/">Home</a></li>
+        </ul>
+      </li>
+
+
+      <li>
         <ul class="sub-menu blank">
           <li><a class="link_name" href="#">Category</a></li>
         </ul>
@@ -52,30 +60,7 @@ export const Navbar = () => {
           <li><a href="#">PHP & MySQL</a></li>
         </ul>
       </li>
-      <li>
-        <div class="iocn-link">
-          <a href="#">
-            <i class='bx bx-book-alt' ></i>
-            <span class="link_name">Posts</span>
-          </a>
-          <i class='bx bxs-chevron-down arrow' ></i>
-        </div>
-        <ul class="sub-menu">
-          <li><a class="link_name" href="#">Posts</a></li>
-          <li><a href="#">Web Design</a></li>
-          <li><a href="#">Login Form</a></li>
-          <li><a href="#">Card Design</a></li>
-        </ul>
-      </li>
-      <li>
-        <a href="#">
-          <i class='bx bx-pie-chart-alt-2' ></i>
-          <span class="link_name">Analytics</span>
-        </a>
-        <ul class="sub-menu blank">
-          <li><a class="link_name" href="#">Analytics</a></li>
-        </ul>
-      </li>
+
       <li>
         <a href="#">
           <i class='bx bx-line-chart' ></i>
@@ -85,39 +70,7 @@ export const Navbar = () => {
           <li><a class="link_name" href="#">Chart</a></li>
         </ul>
       </li>
-      <li>
-        <div class="iocn-link">
-          <a href="#">
-            <i class='bx bx-plug' ></i>
-            <span class="link_name">Plugins</span>
-          </a>
-          <i class='bx bxs-chevron-down arrow' ></i>
-        </div>
-        <ul class="sub-menu">
-          <li><a class="link_name" href="#">Plugins</a></li>
-          <li><a href="#">UI Face</a></li>
-          <li><a href="#">Pigments</a></li>
-          <li><a href="#">Box Icons</a></li>
-        </ul>
-      </li>
-      <li>
-        <a href="#">
-          <i class='bx bx-compass' ></i>
-          <span class="link_name">Explore</span>
-        </a>
-        <ul class="sub-menu blank">
-          <li><a class="link_name" href="#">Explore</a></li>
-        </ul>
-      </li>
-      <li>
-        <a href="#">
-          <i class='bx bx-history'></i>
-          <span class="link_name">History</span>
-        </a>
-        <ul class="sub-menu blank">
-          <li><a class="link_name" href="#">History</a></li>
-        </ul>
-      </li>
+
       <li>
         <a href="#">
           <i class='bx bx-cog' ></i>
@@ -128,6 +81,9 @@ export const Navbar = () => {
         </ul>
       </li>
       <li>
+
+
+
     <div class="profile-details">
       <div class="profile-content">
         <img src={GamingLogo} alt={GamingLogo} />
@@ -138,6 +94,8 @@ export const Navbar = () => {
       </div>
       <i class='bx bx-log-out' ></i>
     </div>
+
+
   </li>
 </ul>
   </div>
