ae2dd1804719ca9f432365df0a44c7a556c31653
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-02-15 19:12:49 -0500

    Formulario de logeo terminado temporalmente

--- src/components/page/SingUp/SingUp.jsx
+++ src/components/page/SingUp/SingUp.jsx
@@ -16,7 +16,7 @@ export const SingUp = () => {
     <>
       <div className='contRegAndLog'>
         
-        <div className="contInfotransL">
+        {/* <div className="contInfotransL">
 
           <div className="contTitleInf">
             <h2>¿DO YOU ALREADY HAVE AN ACCOUNT?</h2>
@@ -28,7 +28,7 @@ export const SingUp = () => {
               that you have previously registered by clicking on the following button
             </p>
           </div>
-          
+
           <div className="contBtnLogTrans">
             <button className='BtnLogTrans'>go to login</button>
           </div>
@@ -76,22 +76,74 @@ export const SingUp = () => {
             </div>
 
           </div>
+        </div> */}
+
+
+
+
+
+
+      <div className="contFormRegister">
+
+        <div className="FormRegister">
+          <h2>Login</h2>
+
+          <div className="contIcons">
+
+            <div className="border-icono">
+              <box-icon type='logo' name='instagram'></box-icon>
+            </div>
+
+            <div className="border-icono">
+              <box-icon type='logo' name='facebook-circle'></box-icon>
+            </div>
+
+            <div className="border-icono">
+              <box-icon name='linkedin' type='logo' ></box-icon>
+            </div>
+
+          </div>
+
+          <div className="contInpustForM">
+
+            <div className="boxInput">
+              <input type="email" placeholder='Email'/>
+            </div>
+
+            <div className="boxInput">
+              <input type="password" placeholder='Password'/>
+            </div>
+
+          </div>
+
+          <div className="contBtnForm">
+            <button className='BtnForm'>Login</button>
+          </div>
+
         </div>
+        </div>
+
+        <div className="contInfotransL">
+
+          <div className="contTitleInf">
+            <h2>¿Do not you have an account yet?</h2>
+          </div>
+
+          <div className="contTextInf">
+            <p>If you do not have an account yet, you must complete the registration
+              process to create it, and thus enjoy our entire website
+            </p>
+          </div>
+
+          <div className="contBtnLogTrans">
+            <button className='BtnLogTrans'>go sign up</button>
+          </div>
+
+        </div>
+
+
               
       </div>
     </>
   )
 }
-
-
-{/* <div className="iconos">
-          <div className="border-icono">
-            <box-icon type='logo' name='instagram'></box-icon>
-          </div>
-          <div className="border-icono">
-            <box-icon type='logo' name='facebook-circle'></box-icon>
-          </div>
-          <div className="border-icono">
-            <box-icon name='linkedin' type='logo' ></box-icon>
-          </div>
-        </div> */}
